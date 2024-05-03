92906e0f0f3ae0c4f8d2342c5f560b0864c24b53
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-05-04 01:18:55 +0900

    Refactor : img필드 text -> url Field 변경진행

--- image_url/models.py
+++ image_url/models.py
@@ -4,7 +4,7 @@ from core.models import TimeStampedModel
 class ImageUrl(TimeStampedModel):
     id = models.IntegerField(primary_key=True)
     description = models.CharField(max_length=100, blank=True, null=True)
-    image_url = models.TextField(db_column='imageUrl', blank=True, null=True)  # Field name made lowercase.
+    image_url = models.URLField(db_column='imageUrl', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         db_table = 'image_url'
