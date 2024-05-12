fd59b65399db7be14b02a9f2d61000570811b6b0
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-05-13 00:07:19 +0900

    Feat : Develop...ing

--- image_url/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import ImageUrl
-
-
-class ImageUrlSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = ImageUrl
-        fields = ['image_url']
--- team/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TeamDetailView
-
-
-urlpatterns = [# 클럽 목록 조회 API
-    path('team/<int:pk>/', TeamDetailView.as_view(), name='team-detail'), # 팀 상세 정보 API
-]
