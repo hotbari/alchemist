b2b7a7e8fee4f64c875325ac59043977ac191260
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-05-15 00:27:09 +0900

    feat : tier 정의

--- tier/admin.py
+++ tier/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Tier
 
-# Register your models here.
+admin.site.register(Tier)
--- tier/models.py
+++ tier/models.py
@@ -3,9 +3,11 @@ from matchtype.models import MatchType
 from core.models import TimeStampedModel, SoftDeleteModel
 class Tier(TimeStampedModel, SoftDeleteModel):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=6, blank=True, null=True)
-    level = models.IntegerField(blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
     match_type = models.ForeignKey(MatchType, models.DO_NOTHING)
+    
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'tier'
