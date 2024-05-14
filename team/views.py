d2edc1046bfc3e5f0cfa0e2be9116ac1b115ade1
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-05-14 12:05:12 +0900

    feat : 클럽/팀 유저전체목록 기능 추가

--- team/views.py
+++ team/views.py
@@ -14,11 +14,11 @@ class TeamDetailView(APIView):
             team = Team.objects.get(pk=pk)
             team_serializer = TeamDetailSerializer(team)
 
-            # 팀에 속한 유저 정보 가져오기 (코치로 등록된 유저 제외)
-            users = CustomUser.objects.filter(team=team)
+            # 팀에 속한 상위 3명 유저 정보 가져오기
+            users = CustomUser.objects.filter(team=team).order_by('id')[:3] # 추후에 order_by('-score')[:3] 으로 변경
             user_serializer = UserWithTeamInfoSerializer(users, many=True)
 
-            # 클럽 정보와 함께 코치, 팀, 유저 정보 포함하여 응답
+            
             response_data = {
                 'team': team_serializer.data,
                 'users': user_serializer.data
@@ -27,3 +27,28 @@ class TeamDetailView(APIView):
             return Response(response_data)
         except Team.DoesNotExist:
             return Response({'error': '해당팀이 존재하지 않습니다.'}, status=404)
+        
+        
+        
+class TeamUsersListView(APIView):
+    """
+    팀에 속한 유저 정보를 모두 나타내는 API
+    """
+    def get(self, request, pk):
+        try:
+            # 클럽 객체 가져오기
+            team = Team.objects.get(pk=pk)
+            
+            # 클럽에 속한 유저 정보 가져오기
+            users = CustomUser.objects.filter(team=team).order_by('id')
+            user_serializer = UserWithTeamInfoSerializer(users, many=True)
+
+            # 유저 정보 포함하여 응답
+            return Response(user_serializer.data)
+        except Team.DoesNotExist:
+            return Response({'error': '해당 팀이 존재하지 않습니다.'}, status=404)
+        
+        
+        
+        
+
--- users/serializers.py
+++ users/serializers.py
@@ -87,6 +87,68 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 
 
+
+# 회원정보 수정 serializer
+class UpdateUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+    club = serializers.PrimaryKeyRelatedField(queryset=Club.objects.all(), required=False)
+    image_file = serializers.ImageField(write_only=True, required=False)
+    image_url = serializers.SerializerMethodField(read_only=True)  # 읽기 전용 이미지 URL 필드 추가
+
+    class Meta:
+        model = User
+        fields = ('password', 'username', 'birth', 'gender', 'club', 'image_file', 'image_url')
+
+    def get_image_url(self, obj):
+        if obj.image_url:
+            return obj.image_url.image_url
+        return None
+
+    def update(self, instance, validated_data):
+        image_data = validated_data.pop('image_file', None)
+        password = validated_data.pop('password', None)
+
+        # 사용자 기본 정보 업데이트
+        instance.username = validated_data.get('username', instance.username)
+        instance.birth = validated_data.get('birth', instance.birth)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.club = validated_data.get('club', instance.club)
+
+        if password:
+            instance.set_password(password)
+
+        # 빈 이미지 파일이 제공된 경우 (이미지 삭제 요청으로 간주)
+        if image_data in [None, '']:
+            if hasattr(instance, 'image_url') and instance.image_url:
+                instance.image_url.delete()  # ImageUrl 인스턴스 삭제
+                instance.image_url = None  # User 모델의 참조 제거
+        # 유효한 이미지 파일이 제공된 경우
+        elif image_data:
+            uploader = S3ImageUploader()
+            file_url, extension, size = uploader.upload_file(image_data)
+
+            # 기존 이미지가 있으면 업데이트, 없으면 새로 생성
+            if hasattr(instance, 'image_url') and instance.image_url:
+                instance.image_url.image_url = file_url
+                instance.image_url.extension = extension
+                instance.image_url.size = size
+                instance.image_url.save()
+            else:
+                image_instance = ImageUrl.objects.create(
+                    image_url=file_url,
+                    extension=extension,
+                    size=size
+                )
+                instance.image_url = image_instance
+
+        instance.save()
+        return instance
+
+
+
+
+
+
 # 유저 상세정보 serializer
 class UserInfoSerializer(serializers.ModelSerializer):
     image_url = ImageUrlSerializer(read_only=True)  # ImageUrl 모델에 대한 시리얼라이저를 사용
