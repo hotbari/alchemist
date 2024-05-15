8ddf9f349d144cc645449565d7405654b3157168
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-05-15 22:09:44 +0900

    feat : matchtype admin.py 수정

--- matchtype/models.py
+++ matchtype/models.py
@@ -6,6 +6,7 @@ class MatchType(TimeStampedModel):
         ('male', '남자'),
         ('female', '여자'),
         ('mix', '혼성'),
+        ('team','팀'),
     )
     
     TYPE_CHOICES = (
@@ -13,7 +14,7 @@ class MatchType(TimeStampedModel):
         ('duo', '복식'),
         ('team', '팀')
     )
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES, null=True)
     type = models.CharField(max_length=6, choices=TYPE_CHOICES, null=True)
     
