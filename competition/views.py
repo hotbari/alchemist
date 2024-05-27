d3509668e84d7038123155344fcff725e0388417
Yongdae92 <dydals0415@naver.com>
2024-05-27 12:40:23 +0900

    fix: 대회신청부분 중복신청 예외처리 수정

--- competition/views.py
+++ competition/views.py
@@ -25,7 +25,7 @@ class CompetitionListView(APIView):
     permission_classes = [AllowAny]
     
     def get(self, request):
-        # 쿼리 파라미터로부터 gender와 type을 받아옵니다.
+        # 쿼리 파라미터로부터 gender와 type 가져옴
         gender = request.query_params.get('gender')
         type = request.query_params.get('match_type')
 
@@ -76,8 +76,8 @@ class CompetitionApplyView(APIView):
         
         applicant = request.user # 신청자 = 로그인한 유저
         # 신청자 중복 신청 확인
-        # if Applicant.objects.filter(applicant_info__competition=competition, user=applicant).exists():
-        #     return Response({'error': '해당 대회에 이미 신청하셨습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        if Applicant.objects.filter(applicant_info__competition=competition, user=applicant).exists():
+            return Response({'error': '해당 대회에 이미 신청하셨습니다.'}, status=status.HTTP_400_BAD_REQUEST)
                         
         
         
@@ -114,9 +114,9 @@ class CompetitionApplyView(APIView):
                 return Response({'error': '파트너 부가 달라 신청 불가능합니다.'}, status=status.HTTP_400_BAD_REQUEST)
             
             
-            # # 파트너 중복 신청 확인
-            # elif partner_id and Applicant.objects.filter(applicant_info__competition=competition, user_id=partner_id).exists():
-            #     return Response({'error': '선택하신 파트너는 이미 해당 대회를 신청하셨습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+            # 파트너 중복 신청 확인
+            elif partner_id and Applicant.objects.filter(applicant_info__competition=competition, user_id=partner_id).exists():
+                return Response({'error': '선택하신 파트너는 이미 해당 대회를 신청하셨습니다.'}, status=status.HTTP_400_BAD_REQUEST)
             
             return self.handle_doubles(request, competition, applicant, partner)
         
@@ -130,15 +130,17 @@ class CompetitionApplyView(APIView):
     
     ### 단식 신청 처리 로직
     def handle_singles(self, request, competition, applicant):
+        
+        # 대기 처리
         current_applicants_count = ApplicantInfo.objects.filter(competition=competition).count()
         max_participants = competition.max_participants
         waiting_number = None
         
         if current_applicants_count >= max_participants:
-            # 간단히 대기 번호를 계산
             max_waiting_number = ApplicantInfo.objects.filter(competition=competition, waiting_number__isnull=False).count()
             waiting_number = max_waiting_number + 1
-                
+        
+        # applicant_info 저장        
         applicant_info_data = {
                     'competition': competition.id,
                     'waiting_number': waiting_number,
@@ -151,12 +153,15 @@ class CompetitionApplyView(APIView):
         else:
                 return Response(ApplicantInfoSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+        
+        # applicant 저장
         expired_date = applicant_info_data['expired_date']
         applicant_data = {'user': applicant.id, 'applicant_info': applicant_info.id}
         applicant_serializer = ApplicantSerializer(data=applicant_data)
         if applicant_serializer.is_valid():
             applicant_serializer.save()
             
+            # 대기/정상 신청 응답
             applicant_info_status = '대기신청 완료' if waiting_number else '신청 완료'
             competition_serializer = CompetitionApplySerializer(competition)
             response_data = {
@@ -179,13 +184,12 @@ class CompetitionApplyView(APIView):
     def handle_doubles(self, request, competition, applicant, partner):
         with transaction.atomic(): # 2개 신청 동시 처리
             
-            # 해당 대회 신청된 참가정보 확인 후 대기여부 판별
+            #대기 처리
             current_applicants_count = ApplicantInfo.objects.filter(competition=competition).count()
             max_participants = competition.max_participants
             waiting_number = None
             
             if current_applicants_count >= max_participants:
-                # 간단히 대기 번호를 계산
                 max_waiting_number = ApplicantInfo.objects.filter(competition=competition, waiting_number__isnull=False).count()
                 waiting_number = max_waiting_number + 1
             
