8b7728cb5858a2588a974d17458636ae2837f97c
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-05-28 17:18:00 +0900

    refactor : 클럽신청 상태 부분 코드변경

--- club_applicant/models.py
+++ club_applicant/models.py
@@ -8,10 +8,10 @@ class ClubApplicant(SoftDeleteModel, TimeStampedModel):
     user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
     club = models.ForeignKey(Club, on_delete=models.DO_NOTHING)
     date_applied = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=255, choices=(('pending', '대기'), ('approved', '승인')), default='pending')
+    status = models.CharField(max_length=255, choices=(('pending', '대기'), ('approved', '승인'), ('rejected', '거절')), default='pending')
 
     def __str__(self):
-        return f"{self.user.username} - {self.club.name}"
+        return f'{self.user.username} - {self.club.name}'
     
     
     class Meta:
